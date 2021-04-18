fd4e5c020bcbcf76f0a67ecd2f90265fd1a0d6e3
Björn <78762936+bashFish@users.noreply.github.com>
2021-04-18 13:00:05 +0200

    storehouse for more carrier :D

--- code/building/factory.py
+++ code/building/factory.py
@@ -1,5 +1,6 @@
 from building.barack import BarackFactory
 from building.sawmill import SawmillFactory
+from building.storehouse import StorehouseFactory
 
 
 class Factory:
@@ -8,6 +9,7 @@ class Factory:
         print(config)
         self.barack_factory = BarackFactory(self.config['Barack'])
         self.sawmill_factory = SawmillFactory(self.config['Sawmill'])
+        self.storehouse_factory = StorehouseFactory(self.config['Storehouse'])
 
     def create_building(self, name, coordinate):
         if name == "Barack":
@@ -16,3 +18,5 @@ class Factory:
             return None
         if name == "Sawmill":
             return self.sawmill_factory.instanciate(coordinate)
+        if name == "Storehouse":
+            return self.storehouse_factory.instanciate(coordinate)
---
+++ code/building/storehouse.py
@@ -0,0 +1,39 @@
+import itertools
+
+from building.building import Building, BuildingFactory
+
+from events import GameEvent
+
+
+class StorehouseFactory(BuildingFactory):
+    def __init__(self, config):
+        self._config = config
+
+    def instanciate(self, coordinate):
+        return Storehouse(self._config, coordinate)
+
+
+class Storehouse(Building):
+    def __init__(self, config, coordinate):
+        self.config = config
+        self.coordinate = coordinate
+
+        self.finished = False
+        self.constructionTicks = self.config['construction']['ticks']
+        self.requiredMaterial = list(itertools.chain(*[[c]*self.config['construction']['cost'][c] for c in self.config['construction']['cost']]))
+        self.constructionMaterial = []
+        self.workMaterial = []
+
+    def tick(self, state):
+        if self.finished:
+           pass
+
+        else:
+            if self.constructionMaterial or not self.requiredMaterial:
+                self.constructionTicks -= 1
+                if self.constructionTicks <= 0:
+                    self.finished = True
+                    #TODO: should a building be able to throw an event???
+                    state.add_game_event(GameEvent._ADD_BUILDING, (self.coordinate, self.__class__.__name__))
+            if self.requiredMaterial and state.carrierAvailable() and state.materialAvailable(self.requiredMaterial[0]):
+                self.constructionMaterial.append(state.acquireMaterial(self.requiredMaterial.pop(0)))
