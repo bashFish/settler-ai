df58aec55a52729c882baf1c616dae7b7f26e8ae
Björn <78762936+bashFish@users.noreply.github.com>
2021-04-19 21:12:09 +0200

    added todos

--- training/randomPlayer.py
+++ training/randomPlayer.py
@@ -11,12 +11,15 @@ buildings, key_to_building, objectid_to_building = parse_buildings()
 rows, cols = 50, 50
 
 all_keys = list(key_to_building.keys())
-all_keys.extend(['d', 'q'])
+all_keys.extend(['d', 'q', '-'])
 
 
+#1) learn valid moves
+#2) learn basic objectives: wood->cutter around/ wood -> sawmill -> plank/ barack extends
+#3) learn strategies: all needs planks -> need pipeline/  -> find all wood + reach all area = top score
 
 def enqueue_random_action(state):
-    #TODO: start directly with search tree! only valid moves for starters.
+    #TODO: start directly with search tree! only valid moves for starters (?)
     key = random.choice(all_keys)
     x, y = random.randint(0,50), random.randint(0,50)
     cell = (x,y)
@@ -26,9 +29,9 @@ def enqueue_random_action(state):
         state.add_game_event(GameEvent.DROP, cell)
     elif key == 'q':
         state.add_game_event(GameEvent.END_GAME)
-    else:
+    elif key != '-':
         state.add_game_event(GameEvent.CONSTRUCT_BUILDING, (cell, key_to_building[key]))
-
+    # othw: do nothing/ wait
 
 
 #TODO: copied configs!
@@ -41,4 +44,4 @@ if __name__ == '__main__':
     for r in g.yieldloop():
         enqueue_random_action(s)
 
-    print("Final score: %s/ %s"%(s.get_score(),s.get_ticks()))
+    print("Final score: %s/ %s" % (s.get_score(), s.get_ticks()))
