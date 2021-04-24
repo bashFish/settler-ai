b28f801423f67a22a699bda723c222f220aea6eb
Björn <78762936+bashFish@users.noreply.github.com>
2021-04-24 13:15:46 +0200

    awesome best-player battler - represents top 10 games seen so far

--- training/randomPlayer.py
+++ training/randomPlayer.py
@@ -107,7 +107,7 @@ print(len(all_keys_model))
 
 
 #TODO: maybe ai needs really random moves to find buildable locations?
-def enqueue_random_valid_action(state):
+def generate_random_valid_action(state):
     #TODO: start directly with search tree! only valid moves for starters (?)
     #key = random.choice(all_keys)
 
@@ -123,13 +123,12 @@ def enqueue_random_valid_action(state):
 
     #elif key == 'q':
     #    state.add_game_event(GameEvent.END_GAME)
-    elif category > .4: #key != '-':
+    elif category > .6: #key != '-':
         key = random.choice(list(key_to_building.keys()))
         possible = np.where(state.get_owned_terrain() == 1)
         position = random.randint(0, len(possible[0])-1)
         x, y = possible[0][position], possible[1][position]
         cell = (x, y)
-        state.add_game_event(GameEvent.CONSTRUCT_BUILDING, (cell, key_to_building[key]))
     else:
         key = '-'
         cell = None
@@ -200,6 +199,25 @@ def prediction_to_coords(preds):
     cell = (int(min(abs(coords[0]) , 49)), int(min(abs(coords[1]) , 49)))
     return cell
 
+# top 10 moves of top 10 games
+top_10_games = [(-100000,[])]*10
+top_building_coords = {}
+
+#TODO: this is going to be replaced by a target player!
+# let ais play against each other
+# this player does not generalize to other maps!
+def get_best_player_move_randomized(state, move):
+    #highest = max((sc[0], it) for it, sc in enumerate(top_10_games))
+    highest = top_10_games[-1]
+    if random.random() > .9 or highest[0] < 0:   # 5 % total random moves
+        key, cell = generate_random_valid_action(state)
+    else:
+        current = -1
+        while current > -8 and top_10_games[current-1][0] > 0 and random.random() > .7:
+            current -= 1
+        key,_, cell = top_10_games[current][1][move]
+
+    return key, cell
 
 #TODO: copied configs!
 if __name__ == '__main__':
@@ -225,13 +243,16 @@ if __name__ == '__main__':
         i = 1
         for r in g.yieldloop():
             print("\t %s \t %s \t %s %s" % (j, i, s.get_score(), s.get_state_dict()))
-            predict_key, coords = None, None
+            predict_key, coords = '-', None
 
-            if i < 11:
+            if i < 10:
                 # TODO: Use epsilon-greedy for exploration
-                if random.random() > 0.8: # 10% random moves
-                    print("random")
-                    predict_key, coords = enqueue_random_valid_action(s)
+                if random.random() > 0.2: # 80% best player / random moves
+                    predict_key, coords = get_best_player_move_randomized(s, i)
+                    print("random %s %s"%(predict_key, coords))
+                    if predict_key != '-':
+                        s.add_game_event(GameEvent.CONSTRUCT_BUILDING, (coords, key_to_building[predict_key]))
+
                     for ijk in range(5):
                         if all_keys_model[ijk] == predict_key:
                             building = tf.one_hot(ijk, 5)
@@ -252,7 +273,7 @@ if __name__ == '__main__':
                     if predict_key != '-':
                         prediction_2 = m_coords.predict(state_representation_vector_2(s, old_gambled_states, building))
                         coords = prediction_to_coords(prediction_2[0])
-                        #print(" pred %s %s" %(prediction_2, cell))
+
                         while not s.check_coordinates_buildable(coords):
                             possible = np.where(s.get_owned_terrain() == 1)
                             position = random.randint(0, len(possible[0]) - 1)
@@ -263,7 +284,7 @@ if __name__ == '__main__':
                     print(" predicted %s %s"%(predict_key, coords))
 
             action_input.append(state_representation_vector(s, old_gambled_states)[0])
-            if predict_key != '-':
+            if predict_key and predict_key != '-':
                 coord_input.append(state_representation_vector_2(s, old_gambled_states, building))
             else:
                 coord_input.append(None)
@@ -274,10 +295,11 @@ if __name__ == '__main__':
             if i % 50 == 0:
                 move_matrices = tf.one_hot([c[1] for c in action_history[:10]], 5)
                 updated_q_values = np.array(score_history[5:15])-np.array(score_history[:10])+(np.array(score_history[35:45])-np.array(score_history[:10]))*2
+
                 with tf.GradientTape() as tape:
                     tape.watch(m_action.trainable_variables)
                     q_values = m_action(np.array([action_input[:10]]))
-                    q_action = tf.reduce_sum(tf.multiply(q_values[:, :, 10:], move_matrices),axis=2)
+                    q_action = tf.reduce_sum(tf.multiply(q_values[:, :, 10:], move_matrices),axis=2) #TODO: could be trained differently / wo multiply but abs distance
 
                     loss = loss_function(updated_q_values, q_action)
                     grads = tape.gradient(loss, m_action.trainable_variables)
@@ -287,13 +309,12 @@ if __name__ == '__main__':
                 current_coords_in = []
                 current_updated_q = []
                 target_coords = []
-                for histi, histk in enumerate(action_history):
-                    if histi > 9:
-                        break
+                for histi, histk in enumerate(action_history[:10]):
                     if coord_input[histi] is not None:
                         current_coords_in.append(coord_input[histi])
                         current_updated_q.append(score_history[histi+10]-score_history[histi]+(score_history[histi+30]-score_history[histi])*2)
                         target_coords.append(action_history[histi][2])
+
                 if len(current_coords_in):
                     with tf.GradientTape() as tape:
 
@@ -315,16 +336,25 @@ if __name__ == '__main__':
 
                     optimizer.apply_gradients(zip(grads2, m_coords.trainable_variables))
 
+                lowest = min((sc[0],it) for it, sc in enumerate(top_10_games))
+                if lowest[0] < score_history[-1]:
+                    del top_10_games[lowest[1]]
+                    top_10_games.append((score_history[-1], action_history[:10]))
+                    top_10_games.sort()
+
+
                 state_history = [] #state_history[-20:]
                 action_history = [] #action_history[-20:]
                 action_input = [] #action_input[-20:]
                 coord_input = [] #coord_input[-20:]
                 score_history = [] #score_history[-20:]
 
-            #time.sleep(1)
-            i += 1
-            if i > 51: #20*moves_per_game+1:
                 break
+
+            i += 1
+            #time.sleep(1)
+            #if i > 51: #20*moves_per_game+1:
+            #    break
 
 
         #print("Final score: %s/ %s" % (s.get_score(), s.get_ticks()))
