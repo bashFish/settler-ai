3582f7944e3ba4508dc318f0decd0484d1903ecb
Björn <78762936+bashFish@users.noreply.github.com>
2021-06-17 17:29:43 +0200

    refactoring #3

--- game/building/storehouse.py
+++ game/building/storehouse.py
@@ -1,8 +1,4 @@
-import itertools
-
 from building.building import Building, BuildingFactory
-
-from events import GameEvent
 
 
 class StorehouseFactory(BuildingFactory):
@@ -12,28 +8,4 @@ class StorehouseFactory(BuildingFactory):
 
 
 class Storehouse(Building):
-    def __init__(self, config, coordinate):
-        self.config = config
-        self.coordinate = coordinate
-
-        self.finished = False
-        self.constructionTicks = self.config['construction']['ticks']
-        self.requiredMaterial = list(itertools.chain(*[[c]*self.config['construction']['cost'][c] for c in self.config['construction']['cost']]))
-        self.constructionMaterial = []
-        self.workMaterial = []
-
-    def tick(self, state):
-        if self.finished:
-           pass
-
-        else:
-            if self.constructionMaterial or not self.requiredMaterial:
-                self.constructionTicks -= 1
-                if self.constructionTicks <= 0:
-                    self.finished = True
-                    #TODO: should a building be able to throw an event???
-                    state.add_game_event(GameEvent._ADD_BUILDING, (self.coordinate, self.__class__.__name__))
-                if self.constructionMaterial:
-                    self.constructionMaterial.pop(0)
-            if self.requiredMaterial and state.carrierAvailable() and state.materialAvailable(self.requiredMaterial[0]):
-                self.constructionMaterial.append(state.acquireMaterial(self.requiredMaterial.pop(0)))
+    pass
